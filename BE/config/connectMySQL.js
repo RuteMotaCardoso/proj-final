2cb7119fe083647e3b28959ec818ae0759c42736
IEUser <rutemcardoso@gmail.com>
2020-07-08 09:52:22 +0100

    Heroku config mySql connect

--- BE/config/connectMySQL.js
+++ BE/config/connectMySQL.js
@@ -1,15 +1,24 @@
 // https://stackoverflow.com/questions/51899725/mysql-fetch-date-without-timezone-offset (para evitar conversão para UTC e subtrair 1h)
 const mysql = require('mysql');
-console.log(process.env.JAWSDB_URL);
-const localSql = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
-	database : 'bdcursoprofissional',
-	timezone : 'Z'
-});
-const herokuSql = mysql.createConnection(process.env.JAWSDB_URL);
+//console.log(process.env.JAWSDB_URL);
+let sqlCon;
+
+if (process.env.JAWSDB_URL != undefined ) {
+	const herokuSql = mysql.createConnection(process.env.JAWSDB_URL);
+	sqlCon = herokuSql;
+}
+else {
+	const localSql = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'root',
+		password : '',
+		database : 'bdcursoprofissional',
+		timezone : 'Z'
+	});
+	sqlCon = localSql;
+}
+console.log(sqlCon);
 
 module.exports = {
-	con: localSql
+	con:sqlCon
 };
